ec61d4cfd91af2a7966da20b991b00367ca4862a
Guido Billi <guidobilli@gmail.com>
2015-11-11 23:51:27 +0100

    seconda commit di prova

--- proveJava/src/main/java/proveJava/Prova.java
+++ proveJava/src/main/java/proveJava/Prova.java
@@ -3,7 +3,7 @@ package proveJava;
 public class Prova {
 
 	public static void main(String[] args) {
-		System.out.println("prova di modifica");
+		System.out.println("prova di modifica 2");
 
 	}
 
